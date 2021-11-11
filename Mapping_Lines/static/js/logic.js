e3d85f5b2e80097d70eea1d5bb2f28f33c33b087
jlixander <88115818+jlixander@users.noreply.github.com>
2021-11-10 23:31:55 -0500

    added a line to the map with 4 airport locations

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -3,7 +3,24 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 // let map = L.map('mapid').setView([40.7, -94.5], 4);
-let map = L.map("mapid").setView([37.09, -95.71], 5)
+let map = L.map("mapid").setView([37.6213, -122.3790], 5)
+
+// Coordinates for each point to be used in the line.
+let line = [
+  [33.9416, -118.4085],
+  [30.1975, -97.6664],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "blue",
+  weight: 4,
+  opacity: 0.5,
+  dashArray: '5, 5',
+  dashOffset: '0'
+}).addTo(map);
 
 // An array containing each city's location, state, and population.
 // let cities = [{
@@ -74,7 +91,7 @@ cityData.forEach(function(city) {
 let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "dark-v10",
+    id: "light-v10",
     accessToken: API_KEY
 });
 
