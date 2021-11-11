acca5a9bf681bb15cdc429d22df16c333f4fdfe4
jlixander <88115818+jlixander@users.noreply.github.com>
2021-11-11 01:58:11 -0500

    added airports from online db, added popups, and dark map layer

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,146 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+// let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map("mapid", {
+  center: [40.7, -94.5],
+  zoom: 4
+});
+
+// Add GeoJSON data.
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
+
+// Grabbing our GeoJSON data.
+// L.geoJson(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   onEachFeature: function(feature, latlng) {
+//     console.log(latlng);
+//     latlng.bindPopup("<h2>" + feature.properties.city + "</h2>");
+//   }
+// }).addTo(map);
+
+// An array containing each city's location, state, and population.
+// let cities = [{
+//   location: [40.7128, -74.0059],
+//   city: "New York City",
+//   state: "NY",
+//   population: 8398748
+// },
+// {
+//   location: [41.8781, -87.6298],
+//   city: "Chicago",
+//   state: "IL",
+//   population: 2705994
+// },
+// {
+//   location: [29.7604, -95.3698],
+//   city: "Houston",
+//   state: "TX",
+//   population: 2325502
+// },
+// {
+//   location: [34.0522, -118.2437],
+//   city: "Los Angeles",
+//   state: "CA",
+//   population: 3990456
+// },
+// {
+//   location: [33.4484, -112.0740],
+//   city: "Phoenix",
+//   state: "AZ",
+//   population: 1660272
+// }
+// ];
+
+// Get data from cities.js (file with array of cities)
+//let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//   console.log(city)
+//   L.circleMarker(city.location, {
+//     radius: (city.population - 200000)/100000,
+//     weight: 4
+//   })
+//   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(map);
+//  });
+
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+//  Add a circle to the map for Los Angeles, California. (radius measured in meters)
+// L.circle([34.0522, -118.2437], {
+//     radius: 100,
+//     color: "black",
+//     fillColor: '#ffffa1'
+//  }).addTo(map);
+
+ //Add circlemarker to map with a pixel radius
+//  L.circleMarker([34.0522, -118.2437], {
+//     radius: 300,
+//     color: "black",
+//     fillColor: '#ffffa1'
+//  }).addTo(map);
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "streets-v11",
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Then we add our 'streets' tile layer to the map.
+//streets.addTo(map);
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/jlixander/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data, {
+     // We turn each feature into a marker on the map.
+   onEachFeature: function(feature, latlng) {
+     console.log(latlng);
+     latlng.bindPopup("<h3>Airport Code: " + feature.properties.faa + "</h3> <hr> <h3>Airport Name: " + feature.properties.name + "</h3>");
+   }
+ }).addTo(map);
+});
